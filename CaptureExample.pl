6b614975b7146090c81ba9f5d337e6a5ab919770
anton <anton@3f9e65d7-54e3-4c48-a158-52a0885c5079>
2013-04-09 11:57:29 +0000

    [88] added declined and error tests for capture
    git-svn-id: svn+ssh://office-admin/home/subversion/PensioClientAPI/trunk@85 3f9e65d7-54e3-4c48-a158-52a0885c5079

--- CaptureExample.pl
+++ CaptureExample.pl
@@ -14,82 +14,106 @@ my $api = new Pensio::PensioAPI($installation_url, $username, $password);
 $api->setLogger(new ExampleStdoutLogger());
 
 
-my $request = new Pensio::Request::InitiatePaymentRequest(
-	amount=>2.33, 
-	orderId=>"capture_"+Pensio::Examples::getRandomOrderId(),
-	terminal=>'Pensio Test Terminal',
-	currency=>'EUR',
-	cardnum=>'4111000011110000',
-	emonth=>'03',
-	eyear=>'2042',
-);
-
-my $initiateResponse = $api->initiatePayment(request => $request);
-
-ok ($initiateResponse->wasSuccessful(), "Successfull initiate!")
-	or diag("Initiate before capture failed..: ",Dumper($initiateResponse));
-
-my $paymentId = $initiateResponse->getPrimaryPayment()->getId();
-
-my $request = new Pensio::Request::CaptureRequest(amount=>2.33, paymentId=>$paymentId);
-
-my $response = $api->capture(request => $request);
-
-ok ($response->wasSuccessful(), "Successfull capture!")
-	or diag("Capture failed..: ",Dumper($response));
+sub initiatePayment {
+	my ($cardnum) = @_;
 	
+	if(not defined $cardnum) {
+		$cardnum = '4111000011110000';
+	}
 
+	my $request = new Pensio::Request::InitiatePaymentRequest(
+		amount=>2.33, 
+		orderId=>"capture_".Pensio::Examples::getRandomOrderId(),
+		terminal=>'Pensio Test Terminal',
+		currency=>'EUR',
+		cardnum=>$cardnum,
+		emonth=>'03',
+		eyear=>'2042',
+	);
 	
+	my $initiateResponse = $api->initiatePayment(request => $request);
 	
-my $request = new Pensio::Request::InitiatePaymentRequest(
-	amount=>2.33, 
-	orderId=>"capture_"+Pensio::Examples::getRandomOrderId(),
-	terminal=>'Pensio Test Terminal',
-	currency=>'EUR',
-	cardnum=>'4111000011110000',
-	emonth=>'03',
-	eyear=>'2042',
-);
+	ok ($initiateResponse->wasSuccessful(), "Successfull initiate!")
+		or diag("Initiate before capture failed..: ",Dumper($initiateResponse));
+		
+	return $initiateResponse->getPrimaryPayment()->getId();
+};
 
-my $initiateResponse = $api->initiatePayment(request => $request);
+subtest 'Capture success test' => sub {
+	
+	my $paymentId = initiatePayment();
+	
+	my $request = new Pensio::Request::CaptureRequest(amount=>2.33, paymentId=>$paymentId);
+	
+	my $response = $api->capture(request => $request);
+	
+	ok ($response->wasSuccessful(), "Successfull capture!")
+		or diag("Capture failed..: ",Dumper($response));
 
-ok ($initiateResponse->wasSuccessful(), "Successfull initiate!")
-	or diag("Initiate before capture failed..: ",Dumper($initiateResponse));
+};	
 
-my $paymentId = $initiateResponse->getPrimaryPayment()->getId();
 
-my $request = new Pensio::Request::CaptureRequest(
-	amount=>2.33, 
-	paymentId=>$paymentId,
-	reconciliationIdentifier=>"my local id",
-	invoiceNumber=>"my invoice number",
-	salesTax=>12.3
-);
+subtest 'Capture declined test' => sub {
+	
+	my $paymentId = initiatePayment("4111000011110766");
+	
+	my $request = new Pensio::Request::CaptureRequest(amount=>2.33, paymentId=>$paymentId);
+	
+	my $response = $api->capture(request => $request);
+	
+	ok (!$response->wasSuccessful(), "Declined capture!")
+		or diag("Capture was not declined: ",Dumper($response));
+};	
 
-$request->orderLines()->add(
-	description => "Product 1",
-	itemId => "Product id 1",
-	quantity => 1.24,
-	taxPercent => 20.0,
-	unitCode => "kg",
-	unitPrice => 123.42,
-	discount => 0.42,
-	goodsType => "item"
-);
-
-$request->orderLines()->add(
-	description => "Product 2",
-	itemId => "Product id 2",
-	quantity => 4,
-	taxPercent => 25.0,
-	unitCode => "",
-	unitPrice => 15423.42,
-	discount => 52.54,
-	goodsType => "item"
-);
-
-my $response = $api->capture(request => $request);
-
-ok ($response->wasSuccessful(), "Successfull capture with all options enabled!")
-	or diag("Capture failed..: ",Dumper($response));
+subtest 'Capture error test' => sub {
+	
+	my $paymentId = initiatePayment("4111000011110767");
+	
+	my $request = new Pensio::Request::CaptureRequest(amount=>2.33, paymentId=>$paymentId);
+	
+	my $response = $api->capture(request => $request);
+	
+	ok (!$response->wasSuccessful(), "Errored capture!")
+		or diag("Capture was not errored: ",Dumper($response));
+};
+	
+subtest 'Capture success with loads of data test' => sub {
+	
+	my $paymentId = initiatePayment();
+	
+	my $request = new Pensio::Request::CaptureRequest(
+		amount=>2.33, 
+		paymentId=>$paymentId,
+		reconciliationIdentifier=>"my local id",
+		invoiceNumber=>"my invoice number",
+		salesTax=>12.3
+	);
+	
+	$request->orderLines()->add(
+		description => "Product 1",
+		itemId => "Product id 1",
+		quantity => 1.24,
+		taxPercent => 20.0,
+		unitCode => "kg",
+		unitPrice => 123.42,
+		discount => 0.42,
+		goodsType => "item"
+	);
+	
+	$request->orderLines()->add(
+		description => "Product 2",
+		itemId => "Product id 2",
+		quantity => 4,
+		taxPercent => 25.0,
+		unitCode => "",
+		unitPrice => 15423.42,
+		discount => 52.54,
+		goodsType => "item"
+	);
+	
+	my $response = $api->capture(request => $request);
+	
+	ok ($response->wasSuccessful(), "Successfull capture with all options enabled!")
+		or diag("Capture failed..: ",Dumper($response));
+};
 	
