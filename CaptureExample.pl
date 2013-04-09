d2179731faeea902b565b2be7e49e4090a0d3187
anton <anton@3f9e65d7-54e3-4c48-a158-52a0885c5079>
2013-04-09 11:19:57 +0000

    [resolve87] added missing parameters to capture call
    git-svn-id: svn+ssh://office-admin/home/subversion/PensioClientAPI/trunk@81 3f9e65d7-54e3-4c48-a158-52a0885c5079

--- CaptureExample.pl
+++ CaptureExample.pl
@@ -8,7 +8,7 @@ use Pensio::PensioAPI;
 use Pensio::Request::InitiatePaymentRequest;
 use Pensio::Request::CaptureRequest;
 use Data::Dumper;
-use Test::More tests => 2;
+use Test::More tests => 4;
 
 my $api = new Pensio::PensioAPI($installation_url, $username, $password);
 $api->setLogger(new ExampleStdoutLogger());
@@ -16,24 +16,80 @@ $api->setLogger(new ExampleStdoutLogger());
 
 my $request = new Pensio::Request::InitiatePaymentRequest(
 	amount=>2.33, 
-	orderId=>'testOrder',
+	orderId=>Pensio::Examples::getRandomOrderId(),
 	terminal=>'Pensio Test Terminal',
 	currency=>'EUR',
 	cardnum=>'4111000011110000',
 	emonth=>'03',
 	eyear=>'2042',
 );
-print 'InitiatePaymentRequest: ', Dumper($request), "\n";
+
 my $initiateResponse = $api->initiatePayment(request => $request);
 
 ok ($initiateResponse->wasSuccessful(), "Successfull initiate!")
 	or diag("Initiate before capture failed..: ",Dumper($initiateResponse));
+
 my $paymentId = $initiateResponse->getPrimaryPayment()->getId();
+
 my $request = new Pensio::Request::CaptureRequest(amount=>2.33, paymentId=>$paymentId);
-note('CaptureRequest: ', Dumper($request));
+
 my $response = $api->capture(request => $request);
-note('CaptureResponse: ', Dumper($response));
 
 ok ($response->wasSuccessful(), "Successfull capture!")
+	or diag("Capture failed..: ",Dumper($response));
+	
+
+	
+	
+my $request = new Pensio::Request::InitiatePaymentRequest(
+	amount=>2.33, 
+	orderId=>Pensio::Examples::getRandomOrderId(),
+	terminal=>'Pensio Test Terminal',
+	currency=>'EUR',
+	cardnum=>'4111000011110000',
+	emonth=>'03',
+	eyear=>'2042',
+);
+
+my $initiateResponse = $api->initiatePayment(request => $request);
+
+ok ($initiateResponse->wasSuccessful(), "Successfull initiate!")
+	or diag("Initiate before capture failed..: ",Dumper($initiateResponse));
+
+my $paymentId = $initiateResponse->getPrimaryPayment()->getId();
+
+my $request = new Pensio::Request::CaptureRequest(
+	amount=>2.33, 
+	paymentId=>$paymentId,
+	reconciliationIdentifier=>"my local id",
+	invoiceNumber=>"my invoice number",
+	salesTax=>12.3
+);
+
+$request->orderLines()->add(
+	description => "Product 1",
+	itemId => "Product id 1",
+	quantity => 1.24,
+	taxPercent => 20.0,
+	unitCode => "kg",
+	unitPrice => 123.42,
+	discount => 0.42,
+	goodsType => "item"
+);
+
+$request->orderLines()->add(
+	description => "Product 2",
+	itemId => "Product id 2",
+	quantity => 4,
+	taxPercent => 25.0,
+	unitCode => "",
+	unitPrice => 15423.42,
+	discount => 52.54,
+	goodsType => "item"
+);
+
+my $response = $api->capture(request => $request);
+
+ok ($response->wasSuccessful(), "Successfull capture with all options enabled!")
 	or diag("Capture failed..: ",Dumper($response));
 	
--- Pensio/Request/CaptureRequest.pm
+++ Pensio/Request/CaptureRequest.pm
@@ -4,7 +4,58 @@ use strict;
 use warnings;
 use Moose;
 
+use Pensio::Request::OrderLines;
+
+use Hash::Merge qw (merge);
+
 require Pensio::Request::AmountBasedPaymentRequest;
 extends 'Pensio::Request::AmountBasedPaymentRequest';
+
+has 'reconciliationIdentifier' => (
+	isa => 'Str', 
+	is => 'rw',
+	required => 0,
+);
+
+has 'invoiceNumber' => (
+	isa => 'Str', 
+	is => 'rw',
+	required => 0,
+);
+
+has 'salesTax' => (
+	isa => 'Num', 
+	is => 'rw',
+	required => 0,
+);
+
+has 'orderLines' => (
+	isa => 'Pensio::Request::OrderLines', 
+	is => 'rw',
+	required => 0
+);
+
+sub BUILD
+{
+	my ($self, $xml) = @_;
+	
+	$self->orderLines(new Pensio::Request::OrderLines());
+	
+	return $self;
+}
+
+
+sub parameters {
+	my ($self) = @_;
+	
+	my $params = $self->Pensio::Request::AbstractPaymentRequest::parameters();
+	$params->{reconciliation_identifier} = $self->reconciliationIdentifier();
+	$params->{invoice_number} = $self->invoiceNumber();
+	$params->{sales_tax} = $self->salesTax();
+	
+	$params = merge($params, $self->orderLines()->parameters());
+	
+	return $params;
+}
 
 1;
