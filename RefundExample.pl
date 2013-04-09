c786471ba6371df56a3cd16d97970331ad00589c
anton <anton@3f9e65d7-54e3-4c48-a158-52a0885c5079>
2013-04-09 11:47:23 +0000

    [88] added failing tests for refund and refactored the test suite a bit
    git-svn-id: svn+ssh://office-admin/home/subversion/PensioClientAPI/trunk@83 3f9e65d7-54e3-4c48-a158-52a0885c5079

--- RefundExample.pl
+++ RefundExample.pl
@@ -14,44 +14,111 @@ use Test::More tests => 3;
 my $api = new Pensio::PensioAPI($installation_url, $username, $password);
 $api->setLogger(new ExampleStdoutLogger());
 
-my $request = new Pensio::Request::InitiatePaymentRequest(
-	amount=>2.33, 
-	orderId=>"refund_"+Pensio::Examples::getRandomOrderId(),
-	terminal=>'Pensio Test Terminal',
-	currency=>'EUR',
-	cardnum=>'4111000011110000',
-	emonth=>'03',
-	eyear=>'2042',
-);
 
-my $initiateResponse = $api->initiatePayment(request => $request);
+sub initiatePayment {
+	my ($cardnum, $amount) = @_;
+	
+	if(not defined $amount) {
+		$amount = 2.33;
+	}
+	
+	if(not defined $cardnum) {
+		$cardnum = '4111000011110000';
+	}
 
-ok ($initiateResponse->wasSuccessful(), "Successfull initiate!")
-	or diag("Initiate before capture failed..: ",Dumper($initiateResponse));
+	my $request = new Pensio::Request::InitiatePaymentRequest(
+		amount=>$amount, 
+		orderId=>"refund_".Pensio::Examples::getRandomOrderId(),
+		terminal=>'Pensio Test Terminal',
+		currency=>'EUR',
+		cardnum=>$cardnum,
+		emonth=>'03',
+		eyear=>'2042',
+	);
+	
+	my $initiateResponse = $api->initiatePayment(request => $request);
+	
+	ok ($initiateResponse->wasSuccessful(), "Successfull initiate!")
+		or diag("Initiate before capture failed..: ",Dumper($initiateResponse));
+		
+	return $initiateResponse->getPrimaryPayment()->getId();
+}
 
-my $paymentId = $initiateResponse->getPrimaryPayment()->getId();
+sub capture {
+	my ($paymentId, $amount) = @_;
+	
+	if(not defined $amount) {
+		$amount = 2.33;
+	}
+	
+	my $request = new Pensio::Request::CaptureRequest(
+		amount=>2.33, 
+		paymentId=>$paymentId
+	);
+	
+	my $response = $api->capture(request => $request);
+	
+	ok ($response->wasSuccessful(), "Successfull capture!")
+		or diag("Capture failed..: ",Dumper($response));
+}
 
-my $request = new Pensio::Request::CaptureRequest(
-	amount=>2.33, 
-	paymentId=>$paymentId
-);
+sub refund {
 
-my $response = $api->capture(request => $request);
+	my ($paymentId, $amount) = @_;
+	
+	if(not defined $amount) {
+		$amount = 2.33;
+	}
 
-ok ($response->wasSuccessful(), "Successfull capture!")
-	or diag("Capture failed..: ",Dumper($response));
+	my $request = new Pensio::Request::RefundRequest(
+		amount=>2.33, 
+		paymentId=>$paymentId,
+		reconciliationIdentifier=>"my local id"
+	);
+	
+	my $response = $api->refund(request => $request);
+	
+	
+	return $response;
+}
 
+subtest 'Refund test' => sub {
 
+	my $paymentId = initiatePayment();
+	
+	capture($paymentId);
+	
+	my $response = refund($paymentId);
+	
+	ok ($response->wasSuccessful(), "Successfull refund!")
+		or diag("Refund failed..: ",Dumper($response));
+};
 
+subtest 'Refund failing test' => sub {
 
-my $request = new Pensio::Request::RefundRequest(
-	amount=>2.33, 
-	paymentId=>$paymentId,
-	reconciliationIdentifier=>"my local id"
-);
+	my $paymentId = initiatePayment('4111000011110966');
+	
+	capture($paymentId);
+	
+	my $response = refund($paymentId);
+	
+	ok (!$response->wasSuccessful(), "Failing refund!")
+		or diag("Refund did not fail..: ",Dumper($response));
+	
+	is ($response->getMerchantErrorMessage(), "TestAcquirer[transaction.amount=9.66 case][10966]", "Correct error message");
+};
 
-my $response = $api->refund(request => $request);
+subtest 'Refund error test' => sub {
 
-
-ok ($response->wasSuccessful(), "Successfull refund!")
-	or diag("Refund failed..: ",Dumper($response));
+	my $paymentId = initiatePayment('4111000011110967');
+	
+	capture($paymentId);
+	
+	my $response = refund($paymentId);
+	
+	ok (!$response->wasSuccessful(), "Failing refund!")
+		or diag("Refund did not fail..: ".Dumper($response));
+		
+	is ($response->getMerchantErrorMessage(), "TestAcquirer[capture_amount=9.67 case]", "Correct error message");
+		
+};
