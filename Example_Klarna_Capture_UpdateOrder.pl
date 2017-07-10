c6095a5fd4e941d5806a42771601fbd78989766c
emerson <emerson@3f9e65d7-54e3-4c48-a158-52a0885c5079>
2017-07-10 15:54:48 +0000

    [p83] Code examples for Klarna
    git-svn-id: svn+ssh://office-admin/home/subversion/PensioClientAPI/trunk@296 3f9e65d7-54e3-4c48-a158-52a0885c5079

--- Example_Klarna_Capture_UpdateOrder.pl
+++ Example_Klarna_Capture_UpdateOrder.pl
@@ -9,6 +9,8 @@ use ExampleSettings;
 use ExampleStdoutLogger;
 use Pensio::PensioAPI;
 use Pensio::Request::CaptureRequest;
+use Pensio::Request::UpdateOrderRequest;
+use Pensio::Request::OrderLines;
 use Data::Dumper;
 
 my $api = new Pensio::PensioAPI($installation_url, $username, $password);
@@ -16,13 +18,61 @@ $api->setLogger(new ExampleStdoutLogger());
 
 # CAPTURE: =============================================================================
 
-my $paymentId = '156'; # PUT A PAYMENT ID FROM A PREVIOUSLY CREATED ORDER HERE
+my $paymentId = '157'; # PUT A PAYMENT ID FROM A PREVIOUSLY CREATED ORDER HERE
 	
 my $request = new Pensio::Request::CaptureRequest(paymentId=>$paymentId);
 	
 my $response = $api->capture(request => $request);
 	
 if (!$response->wasSuccessful()) {
-	print("Capture was declined: " . $response->getMerchantErrorMessage());
+	print("Capture was declined: " . $response->getMerchantErrorMessage() . "\n");
 }
-		
+
+# UPDATE ORDER: ========================================================================
+
+my $lines = new Pensio::Request::OrderLines();
+
+$lines->add(
+	description => "description 1",
+	itemId => "id 01",
+	quantity => -1,
+	unitPrice => 1.1,
+	goodsType => "item"
+);
+
+$lines->add(
+	description => "new item",
+	itemId => "new id",
+	quantity => 1,
+	unitPrice => 1.1,
+	goodsType => "item"
+);
+
+$request = new Pensio::Request::UpdateOrderRequest (paymentId=>$paymentId, orderLines=>$lines);
+
+$request->orderLines()->add(
+	description => "description 1",
+	itemId => "id 01",
+	quantity => -1,
+	unitPrice => 1.1,
+	goodsType => "item"
+);
+
+$request->orderLines()->add(
+	description => "new item",
+	itemId => "new id",
+	quantity => 1,
+	unitPrice => 1.1,
+	goodsType => "item"
+);
+
+$response = $api->updateOrder(request => $request);
+
+if (!$response->wasSuccessful()) {
+	print("Update order error: " . $response->getErrorMessage() . "\n");
+	print("Update order error code: " . $response->getErrorCode());
+}
+else {
+	print("Update order: successful!");
+}
+
