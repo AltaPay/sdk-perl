07c143b83296628893bb8dc48f3399f826a5d231
Mahad Ashraf <>
2023-03-28 15:45:58 +0500

    Add support for redirect response in reservation, chargeSubscription & reserveSubscriptionCharge

--- Pensio/Response/ReservationResponse.pm
+++ Pensio/Response/ReservationResponse.pm
@@ -7,10 +7,27 @@ use Moose;
 require Pensio::Response::PensioAbstractPaymentResponse;
 extends 'Pensio::Response::PensioAbstractPaymentResponse';
 
+has 'redirect_url' => (
+	is  => 'rw',
+	isa => 'Maybe[Str]',
+	reader => 'getRedirectUrl',
+	required => 0,
+);
+
 sub BUILD
 {
 	my ($self, $xml) = @_;
+	if($self->wasRedirect){
+		$self->redirect_url($xml->{Body}->{RedirectUrl});
+	}
+
 	return $self;
+}
+
+sub wasRedirect
+{
+	my ($self) = @_;
+	return $self->getErrorCode() == '0' && $self->result eq 'Redirect';
 }
 
 1;
